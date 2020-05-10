3bc95c7d37165b03bf87153bde0bc48009336701
xunmengzhixing <2417510657@qq.com>
2020-05-11 00:13:22 +0800

    单链表的addByOrder和update

--- src/SingleLinkedList/SingleLinkedListDemo.java
+++ src/SingleLinkedList/SingleLinkedListDemo.java
@@ -10,10 +10,23 @@ public class SingleLinkedListDemo {
         //创建列表
         SingleLinkedlist singleLinkedlist = new SingleLinkedlist();
         //加入
-        singleLinkedlist.add(hero1);
-        singleLinkedlist.add(hero2);
-        singleLinkedlist.add(hero3);
-        singleLinkedlist.add(hero4);
+//        singleLinkedlist.add(hero1);
+//        singleLinkedlist.add(hero2);
+//        singleLinkedlist.add(hero3);
+//        singleLinkedlist.add(hero4);
+
+
+
+        //有顺序的插入
+        singleLinkedlist.addByOrder(hero1);
+        singleLinkedlist.addByOrder(hero4);
+        singleLinkedlist.addByOrder(hero2);
+        singleLinkedlist.addByOrder(hero3);
+        singleLinkedlist.addByOrder(hero1);
+
+        //修改名字
+        HeroNode newheroNode = new HeroNode(2, "卢", "西澳");
+        singleLinkedlist.update(newheroNode);
 
         //显示
         singleLinkedlist.list();
@@ -62,6 +75,59 @@ class SingleLinkedlist{
             temp =temp.next;
         }
 
+    }
+
+
+    public void addByOrder(HeroNode heroNode){
+        HeroNode temp = head;
+        boolean flag =false;
+        while(true){
+            if(temp.next == null){
+                break;
+            }
+            if(temp.next.no >heroNode.no){
+                break;
+            }else if(temp.next.no ==heroNode.no){
+                flag = true;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        if(flag){
+            System.out.printf("插入的该英雄的编号%d已经存在，插入失败\n",heroNode.no);
+        }else {
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+
+    }
+
+    public void update(HeroNode newHeroNode){
+        if(head.next == null){
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while(true){
+            if(temp == null){
+                break;//已经遍历结束了
+            }
+            if(temp.no == newHeroNode.no){
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+
+        if(flag){
+            temp.name = newHeroNode.name;
+            temp.nickname =newHeroNode.nickname;
+        }else {
+            System.out.println("没有找到");
+        }
     }
 
 
